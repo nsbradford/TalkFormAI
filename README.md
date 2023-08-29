715399d278aa11dee74ead98e1c089ae8659715c
nsbradford <nsbradford@gmail.com>
2023-08-29 12:10:54 -0400

    create react app, plus linter prettier etc

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # LLM-Form
+
 Conversational form creation and responses
+
+_THIS IS A WORK IN PROGRESS_
+
+This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+
+## Contributing
+
+Requires Node
+
+Install
+
+```bash
+yarn
+```
+
+First, run the development server:
+
+```bash
+yarn dev
+```
+
+Open [http://localhost:3000](http://localhost:3000) with your browser to see the result. The page auto-updates as you make edits.
---
+++ src/components/GoogleAnalytics.tsx
@@ -0,0 +1,28 @@
+import Script from 'next/script';
+
+export const GoogleAnalytics = ({
+  GA_TRACKING_ID,
+}: {
+  GA_TRACKING_ID: string | undefined;
+}) => {
+  if (!GA_TRACKING_ID) throw new Error('GA_TRACKING_ID is not defined');
+  return (
+    <>
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+        window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${GA_TRACKING_ID}');
+        `}
+      </Script>
+    </>
+  );
+};
+
+export default GoogleAnalytics;
