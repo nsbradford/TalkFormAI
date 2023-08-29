5d8d17c7c4708b4f05011ba6a11b1436742b8fda
nsbradford <nsbradford@gmail.com>
2023-08-29 13:27:39 -0400

    simple button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,9 +11,9 @@ export default function Home() {
             TalkForm AI
         </h1>
         <h2 className="text-xl font-extrabold md:text-2xl my-8 inline-block ">
-          Create and submit forms with chat. <span className='bg-gradient-to-r from-yellow-400 via-pink-400 to-fuchsia-500 bg-clip-text text-transparent'>It just works.</span>
+          Create and fill forms with chat. <span className='bg-gradient-to-r from-yellow-400 via-pink-400 to-fuchsia-500 bg-clip-text text-transparent'>It just works.</span>
         </h2>
-        <Link href="/forms/new">
+        <Link href="/forms/new" className='inline-block bg-gradient-to-r from-yellow-400 via-pink-400 to-fuchsia-500 text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out hover:from-yellow-300 hover:via-pink-300 hover:to-fuchsia-400'>
           Create a form ›
         </Link>
       </div>
