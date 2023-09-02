47825db47cc592f464792b45fe382fd5a1d85f86
nsbradford <nsbradford@gmail.com>
2023-09-02 18:12:49 -0400

    add user settings

---
+++ src/pages/settings.tsx
@@ -0,0 +1,39 @@
+import { User } from '@/types';
+import { getUserFromSupabase } from '@/utils';
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import { useSessionContext } from '@supabase/auth-helpers-react';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { Database } from '../../types/supabase';
+import Spinner from '@/components/home/Spinner';
+import Page from '@/components/layout/Page';
+
+export default function SettingsMode() {
+  // TODO later refactor all the user hydration code
+  const { push } = useRouter();
+  const { isLoading: isSessionLoading, session, error } = useSessionContext();
+  const supabase = createClientComponentClient<Database>();
+  const [user, setUser] = useState<null | User>(null);
+
+  useEffect(() => {
+    if (!isSessionLoading && !session) {
+      push('/auth');
+    }
+    if (!isSessionLoading && session) {
+      getUserFromSupabase(session, supabase, setUser);
+    }
+  }, [isSessionLoading, session]);
+
+  return (
+    <Page pageTitle="Settings" user={null}>
+      {user ? (
+        <>
+          <h1>Email: {user.email}</h1>
+          <h1>User ID: {user.id}</h1>
+        </>
+      ) : (
+        <Spinner />
+      )}
+    </Page>
+  );
+}
