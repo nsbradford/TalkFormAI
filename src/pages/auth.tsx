fefc46a2692119d8c190bdc6b0777eb6355b104e
nsbradford <nsbradford@gmail.com>
2023-09-02 18:01:02 -0400

    fix

--- src/pages/auth.tsx
+++ src/pages/auth.tsx
@@ -31,7 +31,7 @@ export default function AuthPage() {
 
   return (
     <>
-      <NavBar getAvatar={() => <></>} userNavigation={[]} props={{}} />
+      <NavBar getAvatar={() => <></>} userNavigation={[]} user={null} />
       <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
