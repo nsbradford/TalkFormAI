82e79a4c61a3eb5fdf5f75cef3a04ba57ec62276
nsbradford <nsbradford@gmail.com>
2023-09-02 16:22:26 -0400

    home nearly done

--- src/components/home/DashboardMode.tsx
+++ src/components/home/DashboardMode.tsx
@@ -7,6 +7,7 @@ import { useSessionContext } from '@supabase/auth-helpers-react';
 import Link from 'next/link';
 import { useEffect, useState } from 'react';
 import { Database } from '../../../types/supabase';
+import { titleGradient, titleGradientHover } from '../misc';
 
 export default function DashboardMode(props: { user: User | null }) {
   const { user } = props;
@@ -59,77 +60,94 @@ export default function DashboardMode(props: { user: User | null }) {
           Create a form to get started
         </p>
 
-        <Link href={'/forms/new'}>
-          <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
-            New form
-          </button>
+        <Link
+          href={'/forms/new'}
+          className={`inline-block ${titleGradient} text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out ${titleGradientHover}`}
+        >
+          {/* <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> */}
+          New form
+          {/* </button> */}
         </Link>
       </div>
     );
   } else {
     return (
       <div className="px-4">
-        <Link href={'/forms/new'}>
-          <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600">
-            New form
-          </button>
+        <Link
+          href={'/forms/new'}
+          className={`inline-block ${titleGradient} text-white font-semibold py-2 px-4 rounded-lg transition duration-300 ease-in-out ${titleGradientHover} mb-10`}
+        >
+          {/* <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"> */}
+          New form
+          {/* </button> */}
         </Link>
-        <ul role="list" className="divide-y divide-gray-200 space-y-4">
-          {allForms.map((f) => {
-            const responsesForThisForm = formIdToResponses[f.id] || [];
-            const badgeColor = f.is_open
-              ? 'bg-green-100 text-green-800'
-              : 'bg-red-100 text-red-800';
-            const camelCaseTitle =
-              f.name.charAt(0).toUpperCase() + f.name.slice(1, f.name.length);
+        {allForms.length === 0 ? (
+          <div className="flex flex-col items-center justify-center h-full">
+            <h1 className="text-2xl font-semibold text-gray-900">
+              You have no forms
+            </h1>
+            <p className="mt-2 text-sm text-gray-500">
+              Create a form to get started
+            </p>
+          </div>
+        ) : (
+          <ul role="list" className="divide-y divide-gray-200 space-y-4">
+            {allForms.map((f) => {
+              const responsesForThisForm = formIdToResponses[f.id] || [];
+              const badgeColor = f.is_open
+                ? 'bg-green-100 text-green-800'
+                : 'bg-red-100 text-red-800';
+              const camelCaseTitle =
+                f.name.charAt(0).toUpperCase() + f.name.slice(1, f.name.length);
 
-            return (
-              <li
-                key={f.id}
-                className="flex flex-wrap justify-between gap-4 py-5"
-              >
-                <div className="flex-grow space-y-2">
-                  <div className="flex items-center gap-4">
-                    <p className="text-sm font-semibold leading-6 text-gray-900">
-                      {camelCaseTitle}
+              return (
+                <li
+                  key={f.id}
+                  className="flex flex-wrap justify-between gap-4 py-5"
+                >
+                  <div className="flex-grow space-y-2">
+                    <div className="flex items-center gap-4">
+                      <p className="text-md font-semibold leading-6 text-gray-900">
+                        {camelCaseTitle}
+                      </p>
+                      <span
+                        className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ${badgeColor} ring-1 ring-inset ring-gray-500/10`}
+                      >
+                        {f.is_open ? 'Open' : 'Closed'}
+                      </span>
+                    </div>
+                    <p className="text-xs leading-5 text-gray-500">
+                      {f.description
+                        ? f.description.slice(0, 128) + '...'
+                        : f.raw_instructions.slice(0, 128) + '...'}
                     </p>
-                    <span
-                      className={`inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ${badgeColor} ring-1 ring-inset ring-gray-500/10`}
-                    >
-                      {f.is_open ? 'Open' : 'Closed'}
-                    </span>
                   </div>
-                  <p className="text-xs leading-5 text-gray-500">
-                    {f.description
-                      ? f.description.slice(0, 128) + '...'
-                      : f.raw_instructions.slice(0, 128) + '...'}
-                  </p>
-                </div>
-                <div className="flex flex-col gap-2 mt-4 sm:mt-0">
-                  <div className="flex gap-2">
-                    <Link href={'/forms/' + f.id}>
-                      <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600">
-                        View responses
-                      </button>
-                    </Link>
-                    {f.is_open ? (
-                      <Link href={'/forms/fill/' + f.id}>
+                  <div className="flex flex-col gap-2 mt-4 sm:mt-0">
+                    <div className="flex gap-2">
+                      <Link href={'/forms/' + f.id}>
                         <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600">
-                          <LinkIcon className="h-5 w-5" aria-hidden="true" />
+                          View responses
                         </button>
                       </Link>
-                    ) : null}
+                      {f.is_open ? (
+                        <Link href={'/forms/fill/' + f.id}>
+                          <button className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus:outline-none focus-visible:ring-2 focus-visible:ring-indigo-600">
+                            <LinkIcon className="h-5 w-5" aria-hidden="true" />
+                          </button>
+                        </Link>
+                      ) : null}
+                    </div>
+                    <p className="mt-2 text-xs leading-5 text-gray-500 sm:mt-0">
+                      {responsesForThisForm.length === 0
+                        ? 'No responses yet'
+                        : responsesForThisForm.length + ' responses'}
+                    </p>
                   </div>
-                  <p className="mt-2 text-xs leading-5 text-gray-500 sm:mt-0">
-                    {responsesForThisForm.length === 0
-                      ? 'No responses yet'
-                      : responsesForThisForm.length + ' responses'}
-                  </p>
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     );
   }
