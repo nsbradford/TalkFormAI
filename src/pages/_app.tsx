69a52973986420fe36174bfaee5a30bd45b96545
nsbradford <nsbradford@gmail.com>
2023-09-03 14:35:51 -0400

    posthog

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import GoogleAnalytics from '@/components/GoogleAnalytics';
+import { PostHogScript } from '@/components/posthog';
 import '@/styles/globals.css';
 import { config } from '@fortawesome/fontawesome-svg-core';
 import '@fortawesome/fontawesome-svg-core/styles.css';
@@ -15,6 +16,7 @@ export default function App({ Component, pageProps }: AppProps) {
       <GoogleAnalytics
         GA_TRACKING_ID={process.env.NEXT_PUBLIC_GA_TRACKING_ID}
       />
+      <PostHogScript POSTHOG_API_KEY={process.env.NEXT_PUBLIC_POSTHOG_API_KEY} />
       <SessionContextProvider
         supabaseClient={supabaseClient}
         initialSession={pageProps.initialSession}
