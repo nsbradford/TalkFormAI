eeb2183319a652b126f7347b8f696c1a3d4adfe5
nsbradford <nsbradford@gmail.com>
2023-09-03 16:43:35 -0400

    doppler works

--- playwright.config.ts
+++ playwright.config.ts
@@ -69,11 +69,11 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  //   stdout: 'ignore',
-  //   stderr: 'pipe',
-  // },
+  webServer: {
+    command: 'npm run start',
+    url: 'http://127.0.0.1:3000',
+    reuseExistingServer: !process.env.CI,
+    stdout: 'ignore',
+    stderr: 'pipe',
+  },
 });
